2765a387a01dc4d4431a97889575289ef8e366da
robo-zen <72286586+robo-zen@users.noreply.github.com>
2021-10-03 12:51:21 +0530

    Update divide.cpp
    Fixed the namespace typo and added a return 0

--- divide.cpp
+++ divide.cpp
@@ -1,9 +1,10 @@
 
 #include<iostream>
-using namespce std;
+using namespace std;
 int main(){
   int a,b;
   cout<<"enter two numbers";
   cin>>a>>b;
   cout<<"the division is "<<a/b;
+  return 0;
 }
